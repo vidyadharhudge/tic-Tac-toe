2f056d0494635e9a9d200648fedb4726ccfc8ee2
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-24 20:34:04 +0530

    Added:-Win tie or change the player

--- tic_Tac_toe.sh
+++ tic_Tac_toe.sh
@@ -3,43 +3,88 @@ echo "Welcome To Tic-Tac-Toe Game"
 declare -A  board
 ROW=3
 COLUMN=3
+countOfmoves=0
+totalNumberofMoves=$((ROW*COLUMN))
+
 function resettingBoard()
 {
-   for (( i=1; i<=$ROW; i++ ))
-   do 
+    for (( i=1; i<=$ROW; i++ ))
+    do 
        for (( j=1; j<=$COLUMN; j++ ))
        do
            board[$i,$j]=" "
        done
-   done
+    done
 }
 resettingBoard
 
-function Toss()
+function Toss_And_Assigne_Symbol()
 {
-   Toss=$((RANDOM%2))
-   if [[ $Toss -eq 0 ]]
-   then
-         playerOne=x
-   else
-         playerTwo=0
-   fi
+    Toss=$((RANDOM%2))
+    if [[ $Toss -eq 0 ]]
+    then
+         player=x
+         playerTurn=1
+    else
+         player=0
+         playerTurn=1
+    fi
+        echo $player
 
 }
-Toss
+Toss_And_Assigne_Symbol
 
 function displayBoard()
 {
-   for (( i=1; i<=$ROW; i++ ))
-   do
+     for (( i=1; i<=$ROW; i++ ))
+     do
             echo "---+---+---+"
        for (( j=1; j<=$COLUMN; j++ ))
        do
             echo -e " ${board[$i,$j]} |\c "
        done
             echo
-   done
+     done
             echo "---+---+---+"     
-
 }
 displayBoard
+
+function changePlayer()
+{
+     if [[ $player == x ]]
+     then 
+         player=0
+         playerTurn=1
+     else 
+         player=x
+         playerTurn=1
+     fi
+}
+
+function checkEmpty()
+{
+      if [[ ${board[$2,$3]} == " " ]]
+      then
+          board[$2,$3]=$1
+          displayBoard
+          ((countOfmoves++))
+          changePlayer $1
+      else
+          echo "this poistion is not empty"
+      fi
+}
+
+while [ $countOfmoves -le  $totalNumberofMoves ]
+do
+      if [[ $player == x ]]
+      then
+          read -p "enter the row poistion" rowPoistion
+          read -p "enter the column poistion" columnPoistion
+          checkEmpty $player $rowPoistion $columnPoistion
+
+      else
+          read -p "enter the row poistion" rowPoistion
+          read -p "enter the column poistion" columnPoistion
+          checkEmpty $player $rowPoistion $columnPoistion  
+      fi
+done    
