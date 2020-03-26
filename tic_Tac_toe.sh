f31b1b52a9de325e6a79b5f896fcc421af072ace
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-26 12:21:56 +0530

    Added:-Computer play like me

--- tic_Tac_toe.sh
+++ tic_Tac_toe.sh
@@ -8,9 +8,9 @@ totalNumberofMoves=$((ROW*COLUMN))
 
 function resettingBoard()
 {
-    for (( i=1; i<=$ROW; i++ ))
+    for (( i=0; i<$ROW; i++ ))
     do 
-       for (( j=1; j<=$COLUMN; j++ ))
+       for (( j=0; j<$COLUMN; j++ ))
        do
            board[$i,$j]=" "
        done
@@ -21,27 +21,25 @@ resettingBoard
 function Toss_And_Assigne_Symbol()
 {
     Toss=$((RANDOM%2))
-    if [[ $Toss -eq 0 ]]
-    then
-         player=x
-         playerTurn=1
-    else
-         player=0
-         playerTurn=1
-    fi
-        echo $player
+    case $Toss in
+       1)
+          player=x ;;
+       0)
+          player=o ;;
+     esac
+     echo $player
 
 }
 Toss_And_Assigne_Symbol
 
 function displayBoard()
 {
-     for (( i=1; i<=$ROW; i++ ))
+     for (( i=0; i<$ROW; i++ ))
      do
             echo "---+---+---+"
-       for (( j=1; j<=$COLUMN; j++ ))
+       for (( j=0; j<$COLUMN; j++ ))
        do
-            echo -e " ${board[$i,$j]} |\c "
+            printf " ${board[$i,$j]} |"
        done
             echo
      done
@@ -53,11 +51,9 @@ function changePlayer()
 {
      if [[ $player == x ]]
      then 
-         player=0
-         playerTurn=1
+         player=o
      else 
          player=x
-         playerTurn=1
      fi
 }
 
@@ -67,24 +63,55 @@ function checkEmpty()
       then
           board[$2,$3]=$1
           displayBoard
-          ((countOfmoves++))
+          countOfmoves=$((countOfmoves+1))
           changePlayer $1
       else
           echo "this poistion is not empty"
       fi
 }
 
-while [ $countOfmoves -le  $totalNumberofMoves ]
+function checkwin()
+{
+        
+     for (( i=0; i<$ROW; i++ ))
+     do
+         if [[ ${board[$i,$((i-i))]} == $1 && ${board[$i,$((i+1-i))]} == $1 && ${board[$i,$((i+2-i))]} == $1 ]]     
+         then
+               echo "win"
+         exit
+         fi
+         if [[ ${board[$((i-i)),$i]} == $1 && ${board[$((i+1-i)),$i]} == $1 && ${board[$((i+2-i)),$i]} == $1 ]]
+         then
+               echo "win"
+         exit
+         fi
+    done
+    if [[ ${board[0,0]} == $1 && ${board[1,1]} == $1 && ${board[2,2]} == $1 ]]
+    then
+              echo "win"
+              exit
+    fi
+    if [[ ${board[0,2]} == $1 && ${board[1,1]} == $1 && ${board[2,0]} == $1 ]]       
+    then
+              echo "win"
+              exit
+    fi
+}
+
+while [ $countOfmoves -lt  $totalNumberofMoves ]
 do
       if [[ $player == x ]]
       then
           read -p "enter the row poistion" rowPoistion
           read -p "enter the column poistion" columnPoistion
           checkEmpty $player $rowPoistion $columnPoistion
+          checkwin $player
 
       else
-          read -p "enter the row poistion" rowPoistion
-          read -p "enter the column poistion" columnPoistion
+          Poistion=$((RANDOM%9))
+          rowPoistion=$((Poistion/3))
+          columnPoistion=$((Poistion%3))
           checkEmpty $player $rowPoistion $columnPoistion  
+          checkwin $player
       fi
 done    
