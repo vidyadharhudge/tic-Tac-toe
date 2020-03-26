5ebdbb24af2ca87f51ce8dea4a6859a5d4d9d235
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-26 14:16:17 +0530

    Added:-If i win then i play move

--- tic_Tac_toe.sh
+++ tic_Tac_toe.sh
@@ -23,11 +23,11 @@ function Toss_And_Assigne_Symbol()
     Toss=$((RANDOM%2))
     case $Toss in
        1)
-          player=x ;;
+          gamePlayer=x ;;
        0)
-          player=o ;;
+          gamePlayer=o ;;
      esac
-     echo $player
+     echo $gamePlayer
 
 }
 Toss_And_Assigne_Symbol
@@ -49,69 +49,92 @@ displayBoard
 
 function changePlayer()
 {
-     if [[ $player == x ]]
+     if [[ $gamePlayer == x ]]
      then 
-         player=o
+         gamePlayer=o
      else 
-         player=x
+         gamePlayer=x
      fi
 }
 
 function checkEmpty()
 {
-      if [[ ${board[$2,$3]} == " " ]]
-      then
+     if [[ ${board[$2,$3]} == " " ]]
+     then
           board[$2,$3]=$1
           displayBoard
           countOfmoves=$((countOfmoves+1))
           changePlayer $1
-      else
+     else
           echo "this poistion is not empty"
-      fi
+     fi
+}
+
+function Check_I_Can_Win()
+{
+   for (( row=0; row<$ROW; row++ ))
+   do
+        for (( column=0; column<$COLUMN; column++ ))
+        do
+            if  [[ ${board[$row,$column]} == " " ]]
+            then
+                  board[$row,$column]=$1
+                  checkwin $1
+                  if [[ $win -eq 0 ]]
+                  then
+                       board[$row,$column]=" "
+                  fi
+           fi
+       done
+   done
 }
 
 function checkwin()
 {
-        
+     win=0
      for (( i=0; i<$ROW; i++ ))
      do
          if [[ ${board[$i,$((i-i))]} == $1 && ${board[$i,$((i+1-i))]} == $1 && ${board[$i,$((i+2-i))]} == $1 ]]     
          then
-               echo "win"
-         exit
+                  win=1
          fi
          if [[ ${board[$((i-i)),$i]} == $1 && ${board[$((i+1-i)),$i]} == $1 && ${board[$((i+2-i)),$i]} == $1 ]]
          then
-               echo "win"
-         exit
+                  win=1
          fi
     done
     if [[ ${board[0,0]} == $1 && ${board[1,1]} == $1 && ${board[2,2]} == $1 ]]
     then
-              echo "win"
-              exit
+                  win=1
     fi
     if [[ ${board[0,2]} == $1 && ${board[1,1]} == $1 && ${board[2,0]} == $1 ]]       
     then
-              echo "win"
-              exit
+                  win=1
+    fi
+    if [ $win -eq 1 ]
+    then
+             echo "win $1"
+    exit
+    elif [ $countOfmoves -eq $totalNumberofMoves ]
+    then
+             echo "match is tied"
+    exit
     fi
 }
 
 while [ $countOfmoves -lt  $totalNumberofMoves ]
 do
-      if [[ $player == x ]]
+      if [[ $gamePlayer == x ]]
       then
           read -p "enter the row poistion" rowPoistion
           read -p "enter the column poistion" columnPoistion
-          checkEmpty $player $rowPoistion $columnPoistion
-          checkwin $player
+          checkEmpty $gamePlayer $rowPoistion $columnPoistion
 
       else
+          Check_I_Can_Win
           Poistion=$((RANDOM%9))
           rowPoistion=$((Poistion/3))
           columnPoistion=$((Poistion%3))
-          checkEmpty $player $rowPoistion $columnPoistion  
-          checkwin $player
+          checkEmpty $gamePlayer $rowPoistion $columnPoistion 
       fi
 done    
