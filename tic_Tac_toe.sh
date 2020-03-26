b44fd5cb5dcb96b5f4c48873c33cdd26903d0140
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-26 17:51:32 +0530

    Added:-If Openent WIn Then Block It

--- tic_Tac_toe.sh
+++ tic_Tac_toe.sh
@@ -1,9 +1,10 @@
-#!/bin/bash  
+#!/bin/bash -x  
 echo "Welcome To Tic-Tac-Toe Game"
 declare -A  board
 ROW=3
 COLUMN=3
 countOfmoves=0
+computerPlayer="o"
 totalNumberofMoves=$((ROW*COLUMN))
 
 function resettingBoard()
@@ -25,7 +26,7 @@ function Toss_And_Assigne_Symbol()
        1)
           gamePlayer=x ;;
        0)
-          gamePlayer=o ;;
+          gamePlayer=$computerPlayer ;;
      esac
      echo $gamePlayer
 
@@ -65,6 +66,7 @@ function checkEmpty()
           displayBoard
           countOfmoves=$((countOfmoves+1))
           changePlayer $1
+          checkwin $1
      else
           echo "this poistion is not empty"
      fi
@@ -72,20 +74,38 @@ function checkEmpty()
 
 function Check_I_Can_Win()
 {
+   flag=1
    for (( row=0; row<$ROW; row++ ))
    do
         for (( column=0; column<$COLUMN; column++ ))
         do
             if  [[ ${board[$row,$column]} == " " ]]
             then
-                  board[$row,$column]=$1
-                  checkwin $1
-                  if [[ $win -eq 0 ]]
+                       board[$row,$column]=$1
+                       checkwin $1
+                  if [ $win -eq 0 ]
                   then
                        board[$row,$column]=" "
-                  fi
-           fi
+                  elif [[ $win -eq 1 && ${board[$row,$column]} == $gamePlayer ]]
+                  then
+                       displayBoard
+                       echo "$gamePlayer win"
+                       exit
+                 elif [ $win -eq 1 ]
+                 then
+                       board[$row,$column]=$gamePlayer
+                       displayBoard
+                       win=0
+                       flag=0
+                      ((countOfmoves++))
+                       break  
+                 fi
+            fi
        done
+       if [ $flag -eq 0 ]
+       then
+             break
+       fi
    done
 }
 
@@ -97,29 +117,20 @@ function checkwin()
          if [[ ${board[$i,$((i-i))]} == $1 && ${board[$i,$((i+1-i))]} == $1 && ${board[$i,$((i+2-i))]} == $1 ]]     
          then
                   win=1
-         fi
-         if [[ ${board[$((i-i)),$i]} == $1 && ${board[$((i+1-i)),$i]} == $1 && ${board[$((i+2-i)),$i]} == $1 ]]
+    
+         elif [[ ${board[$((i-i)),$i]} == $1 && ${board[$((i+1-i)),$i]} == $1 && ${board[$((i+2-i)),$i]} == $1 ]]
+         then
+                  win=1
+        
+         elif [[ $i -eq 0 && ${board[$i,$i]} == $1 && ${board[$((i+1)),$((i+1))]} == $1 && ${board[$((i+1)),$((i+1))]} == $1 ]]
+         then
+                  win=1
+    
+         elif [[ ${board[0,2]} == $1 && ${board[1,1]} == $1 && ${board[2,0]} == $1 ]]       
          then
                   win=1
          fi
-    done
-    if [[ ${board[0,0]} == $1 && ${board[1,1]} == $1 && ${board[2,2]} == $1 ]]
-    then
-                  win=1
-    fi
-    if [[ ${board[0,2]} == $1 && ${board[1,1]} == $1 && ${board[2,0]} == $1 ]]       
-    then
-                  win=1
-    fi
-    if [ $win -eq 1 ]
-    then
-             echo "win $1"
-    exit
-    elif [ $countOfmoves -eq $totalNumberofMoves ]
-    then
-             echo "match is tied"
-    exit
-    fi
+         done
 }
 
 while [ $countOfmoves -lt  $totalNumberofMoves ]
@@ -131,10 +142,25 @@ do
           checkEmpty $gamePlayer $rowPoistion $columnPoistion
 
       else
-          Check_I_Can_Win
-          Poistion=$((RANDOM%9))
-          rowPoistion=$((Poistion/3))
-          columnPoistion=$((Poistion%3))
-          checkEmpty $gamePlayer $rowPoistion $columnPoistion 
+          anotherPlayer="x"
+          Check_I_Can_Win $gamePlayer
+          Check_I_Can_Win $anotherPlayer
+          if [ $flag -eq 1 ]
+          then 
+               Poistion=$((RANDOM%9))
+               rowPoistion=$((Poistion/3))
+               columnPoistion=$((Poistion%3))
+               checkEmpty $gamePlayer $rowPoistion $columnPoistion 
+         else
+                  changePlayer $anotherPlayer
+          fi
       fi
-done    
+if [[ $win == 1 ]]
+then
+      break
+fi
+done
+if [[ $win == 0 ]]
+then
+      echo "match is tie"
+fi
