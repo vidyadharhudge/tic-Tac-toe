4383349f0c59c99c21421809e8f287819e1df175
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-13 14:25:08 +0530

    Refactor:-Changeses The Identation And Gave The Proper Messages

--- tic_Tac_toe.sh
+++ tic_Tac_toe.sh
@@ -1,173 +1,168 @@
-#!/bin/bash -x
-echo "Welcome To Tic_Tac_Toe"
-declare -A board
+#!/bin/bash -x 
+echo "welcome to tic_tac_toe"
+declare -A bord
 ROW=3
 COLUMN=3
-countOfmoves=0
-computerPlayer="o"
-totalNumberOfMoves=$((ROW*COLUMN))
-#function for resetting the board
-function resettingBoard()
-{
+COUNTOFMOVES=0
+COMPUTERPLAYER="o"
+TOTALNUMBEROFMOVES=$((ROW*COLUMN))
+#reset the bord
+function resetingBord(){
 	for (( i=0; i<$ROW; i++ ))
 	do
 		for (( j=0; j<$COLUMN; j++ ))
 		do
-			board[$i,$j]=" "
+			bord[$i,$j]=" "
 		done
 	done
 }
-#function for toss and assign the symbol
-function Toss_And_Assign_Symbol()
-{
-	Toss=$((RANDOM%2))
-	case $Toss in
-	1)
-			gamePlayer=x ;;
-	0)
-			gamePlayer=$computerPlayer ;;
+#toss for who play first
+function tossAndAssignSymbol(){
+	toss=$((RANDOM%2))
+	case $toss in
+		1)
+			player=x ;;
+		0)
+			player=$COMPUTERPLAYER ;;
 	esac
-	echo $gamePlayer
+	echo $player
 }
-#function for display the game board
-function displayBoard()
-{
+
+#display the bord 
+function displayBord(){
 	for (( i=0; i<$ROW; i++ ))
 	do
 			echo "---+---+---+"
 		for (( j=0; j<$COLUMN; j++ ))
 		do
-			printf " ${board[$i,$j]} |"
-   	done
-		   echo
+			printf " ${bord[$i,$j]} |"
+		done
+		echo
 	done
-	      echo "---+---+---+"
-
+	echo "---+---+---+"
 }
-#function for check who is win
-function checkwin()
-{
+# winning conditions
+function checkWin(){
    win=0
    for (( i=0; i<$ROW; i++ ))
    do
-      if [[ ${board[$i,$((i-i))]} == $1 && ${board[$i,$((i+1-i))]} == $1 && ${board[$i,$((i+2-i))]} == $1 ]]
+      if [[ ${bord[$i,$((i-i))]} == $1 && ${bord[$i,$((i+1-i))]} == $1 && ${bord[$i,$((i+2-i))]} == $1 ]]
       then
          win=1
-      elif [[ ${board[$((i-i)),$i]} == $1 && ${board[$((i+1-i)),$i]} == $1 &&  ${board[$((i+2-i)),$i]} == $1 ]]
+      elif [[ ${bord[$((i-i)),$i]} == $1 && ${bord[$((i+1-i)),$i]} == $1 &&  ${bord[$((i+2-i)),$i]} == $1 ]]
       then
          win=1
-      elif [[ $i -eq 0 && ${board[$i,$i]} == $1 && ${board[$((i+1)),$((i+1))]} == $1 && ${board[$((i+2)),$((i+2))]} == $1 ]]
+      elif [[ $i -eq 0 && ${bord[$i,$i]} == $1 && ${bord[$((i+1)),$((i+1))]} == $1 && ${bord[$((i+2)),$((i+2))]} == $1 ]]
       then
          win=1
-      elif [[ ${board[0,2]} == $1 && ${board[1,1]} == $1 && ${board[2,0]} == $1 ]]
+      elif [[ ${bord[0,2]} == $1 && ${bord[1,1]} == $1 && ${bord[2,0]} == $1 ]]
       then
          win=1
 	   fi
 	done
 }
-#function for changing the player 
-function changePlayer() 
-{
-	if [[ $gamePlayer == x ]]
-	then
-		gamePlayer=o
-	else
-		gamePlayer=x
-	fi
-}
-#function for check poistion is full or not
-function checkEmpty()
-{
-	if [[ ${board[$2,$3]} == " " ]]
-	then
-		board[$2,$3]=$1
-		displayBoard
-		countOfmoves=$((countOfmoves+1))
-		changePlayer $1
-		checkwin $1
-	else
-		echo "The Poistion is not empty"
-	fi
-}
-#function for Avalibality of corner and center
-function Check_For_Avalibale_Corner_And_Center()
-{
 
+#after evry moves change the player
+function changePlayer(){
+	if [[ $player == x ]]
+	then
+		player=o
+	else
+		player=x
+	fi
+}
+#check the any place empty
+function checkEmpty(){
+	if [[ ${bord[$2,$3]} == " " ]]
+	then
+		bord[$2,$3]=$1
+		displayBord
+		countOfMoves=$((countOfMoves+1))
+		changePlayer $1
+		checkWin $1
+	else
+		echo "invalid place"
+	fi
+}
+#check available corner center and sides are available and place the position
+function availableCornerCenterAndSide(){
+#check for available corner and place position when corner available
 	if [ $flag -eq 1 ]
 	then
 		for (( row=0; row<$ROW; row=$(( row+2 )) ))
 		do
 			for (( column=0; column<$COLUMN; column=$(( column+2 )) ))
 			do
-				if [[ ${board[$row,$column]} == " " ]]
+				if [[ ${bord[$row,$column]} == " " ]]
 				then
-					board[$row,$column]=$gamePlayer
-					displayBoard
+					bord[$row,$column]=$player
+					displayBord
 					flag=0
 					break
 				fi
 			done
 			if [ $flag -eq 0 ]
 			then
-				  break
+				break
 			fi
-	  done
-  fi
-  if [ $flag -eq 1 ]
-  then
-		board[1,1]=$gamePlayer
-		displayBoard
+		done
+	fi
+#if corner not available then go for center
+	if [ $flag -eq 1 ]
+	then
+		bord[1,1]=$player
+		displayBord
 		flag=0
-  fi
-  if [ $flag -eq 1 ]
-  then
-	 for (( row=0;row<$ROW;row++ ))
-	 do
+	fi
+#if center or corner not available then take any available position
+	if [ $flag -eq 1 ]
+	then
+	for (( row=0;row<$ROW;row++ ))
+	do
 		for (( column=0;column<$COLUMN;column++ ))
 		do
-			if [[ ${board[$row,$column]} == " " ]]
+			if [[ ${bord[$row,$column]} == " " ]]
 			then
-				board[$row,$column]=$gamePlayer
-				displayBoard
+				bord[$row,$column]=$player
+				displayBord
 				flag=0
 				break
 			fi
-	  done
-     if [ $flag -eq 0 ]
-	  then
-		     break
-	  fi
-   done
-  fi
+		done
+	if [ $flag -eq 0 ]
+	then
+		break
+	fi
+	done
+	fi
 }
-#function for check i can win 
-function Check_I_Can_Win()
-{
+#check i can win if no then block other user for win
+function checkICanWinThenPlayAndBlock(){
 	flag=1
    for (( r=0; r<$ROW; r++ ))
    do
       for (( c=0; c<$COLUMN; c++ ))
       do
-         if [[ ${board[$r,$c]} == " " ]]
+         if [[ ${bord[$r,$c]} == " " ]]
          then
-               board[$r,$c]=$1
-               checkwin $1
+            bord[$r,$c]=$1
+            checkWin $1
             if [ $win -eq 0 ]
             then
-                board[$r,$c]=" "
-             elif [[ $win -eq 1 && ${board[$r,$c]} == $gamePlayer ]]
-             then
-                displayBoard
-                echo "$gamePlayer win !"
-             exit
-             elif [ $win -eq 1 ]
-             then
-                board[$r,$c]=$gamePlayer
-                displayBoard
-                win=0
-                flag=0
-                ((countOfmoves++))
-                break
+                bord[$r,$c]=" "
+            elif [[ $win -eq 1 && ${bord[$r,$c]} == $player ]]
+            then
+               displayBord
+               echo "$player win !"
+            exit
+            elif [ $win -eq 1 ]
+            then
+               bord[$r,$c]=$player
+               displayBord
+               win=0
+               flag=0
+               ((COUNTOFMOVES++))
+               break
             fi
          fi
       done 
@@ -175,32 +170,31 @@ function Check_I_Can_Win()
 }
 
 
-resettingBoard
-Toss_And_Assign_Symbol
-displayBoard
-
-while [ $countOfmoves -lt $totalNumberOfMoves ]
+resetingBord
+tossAndAssignSymbol
+displayBord
+while [ $COUNTOFMOVES -lt $TOTALNUMBEROFMOVES ]
 do
-	if [[ $gamePlayer == x ]]
+	if [[ $player == x ]]
 	then
 		read -p "enter row position" rowPosition
 		read -p "enter column position" columnPosition
-		checkEmpty $gamePlayer $rowPosition $columnPosition
-	else
-		 AnotherPlayer="x"
-		 Check_I_Can_Win $gamePlayer
-		 heck_I_Can_Win $AnotherPlayer
-		 Check_For_Avalibale_Corner_And_Center $gamePlayer
-	    if [ $flag -eq 1 ]
-	    then
-		     position=$((RANDOM%9))
-		     rowPosition=$((position/3))
-		     columnPosition=$((position%3))
-		     checkEmpty $gamePlayer $rowPosition $columnPosition
-	    else
-		     changePlayer $AnotherPlayer
-	    fi
-	fi
+		checkEmpty $player $rowPosition $columnPosition
+	 else
+		OTHERPLAYER="x"
+		checkICanWinThenPlayAndBlock $player
+		checkICanWinThenPlayAndBlock $OTHERPLAYER
+		availableCornerCenterAndSide $player
+		if [ $flag -eq 1 ]
+		then
+			position=$((RANDOM%9))
+			rowPosition=$((position/3))
+			columnPosition=$((position%3))
+			checkEmpty $player $rowPosition $columnPosition
+		else
+			changePlayer $OTHERPLAYER
+		fi
+	 fi
 if [[ $win == 1 ]]
 then
 	break
